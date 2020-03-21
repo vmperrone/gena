0e6b2ede1c149ef172b31a0194e0a347abef0881
Victor Marino <35439910+c-marinos@users.noreply.github.com>
2020-03-20 18:58:26 -0700

    deleted

--- src/pages.js
+++ src/pages.js
@@ -132,9 +132,6 @@ const pages = [{
   src: 'https://i.imgur.com/5PqRhbe.jpg'
 }, {
   type: 'image',
-  src: 'https://i.imgur.com/f1VB1Lw.jpg'
-}, {
-  type: 'image',
   src: 'https://i.imgur.com/mNhmNxO.jpg'
 }, {
   type: 'image',
