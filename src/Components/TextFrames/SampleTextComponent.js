31e4d5cc7fe24e8dbe2302c1ff5a12ba302c4519
Victor Marino <35439910+c-marinos@users.noreply.github.com>
2020-03-20 19:02:56 -0700

    Link to KS

--- src/Components/TextFrames/SampleTextComponent.js
+++ src/Components/TextFrames/SampleTextComponent.js
@@ -20,7 +20,7 @@ const SampleTextComponent = () => {
           Stay tuned for chapter 2.
         </p>
         <p>
-          In the meantime, please check out the Kickstarter! The rewards are some cute keychains :)
+          In the meantime, please check out the <a href="https://www.kickstarter.com/projects/vmarino/genetic-algorithm-part-2?ref=project_build">Kickstarter</a>! The rewards are some cute keychains :)
         </p>
       </p>
     </div>
